9245ec62a95c0cc6ccc200771e772766e30e6204
aviaviavi <avipress@gmail.com>
2017-07-17 10:04:20 -0700

    updating sha

--- confetti.rb
+++ confetti.rb
@@ -2,7 +2,7 @@ class Confetti < Formula
   desc "A simple config file swapping tool"
   homepage "https://github.com/aviaviavi/confetti"
   url "https://github.com/aviaviavi/confetti/releases/download/0.0.2/confetti-0.0.2-mac.tar.gz"
-  sha256 "649c31c2c2f6e97099518cac28a26b279b4d52c3c6d28a7a53d952e52a3d2bc8"
+  sha256 "1518d34cd100f5ed0ca4311573138fd1b2ce632abe02704a2311a3f9da27ce3f"
 
   bottle :unneeded
 
