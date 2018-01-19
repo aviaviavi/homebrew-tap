eb884d3da9f0ab5ace49e728dc536122de9051f5
aviaviavi <avipress@gmail.com>
2018-01-18 22:33:21 -0800

    add plist

--- sniper.rb
+++ sniper.rb
@@ -14,4 +14,28 @@ class Sniper < Formula
     system "false"
   end
 
+  plist_options :manual => "sniper"
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>KeepAlive</key>
+        <dict>
+          <key>SuccessfulExit</key>
+          <false/>
+        </dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+        </array>
+        <key>KeepAlive</key>
+        <true/>
+      </dict>
+    </plist>
+    EOS
+  end
+
 end
