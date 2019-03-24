852a50b1c93ff135e454a47d268a596077c66346
aviaviavi <avipress@gmail.com>
2019-03-24 09:36:08 -0700

    confetti 1.0.1

--- confetti.rb
+++ confetti.rb
@@ -1,8 +1,8 @@
 class Confetti < Formula
   desc "A simple config file swapping tool"
   homepage "https://github.com/aviaviavi/confetti"
-  url "https://github.com/aviaviavi/confetti/releases/download/1.0.0/confetti-1.0.0-mac.tar.gz"
-  sha256 "aea6d79c3e475d1e0ccf034a1903789a65ef3851b0b9d0c453af408ff40a76fa"
+  url "https://github.com/aviaviavi/confetti/releases/download/1.0.1/confetti-1.0.1-mac.tar.gz"
+  sha256 "b285984bc423a99059605cbcd25f92343647914c86deacada31cbe02e5bd8070"
 
   bottle :unneeded
 
